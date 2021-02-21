11405b826963e3a47017278266d93dcc33158aca
aniket <aniket.kh18@gmail.com>
2021-02-21 15:51:33 +0530

    infi done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Box from "./Components/Box";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Infi from "./Components/Infi";
 
 function App() {
   return (
-    <Box />
+    <Router>
+      <Route path="/" exact component={Box} />
+      <Route path="/infi" exact component={Infi} />
+    </Router>
+   
   );
 }
 
---
+++ src/Components/Infi.js
@@ -0,0 +1,45 @@
+import React,{useState,useEffect} from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+
+function Infi(){
+ 
+
+    const [bigArr,setBigArr] = useState([]);
+    const [smallArr,setSmallArr] = useState(["Hello"]);
+   
+
+    const fetchMoreData = () => {
+        setTimeout(() => {
+            setSmallArr((prev)=>[...prev,"Hello"]);
+        }, 100);
+      };
+    
+
+
+   
+
+
+
+    return(
+        <div>
+        
+        <InfiniteScroll
+          dataLength={smallArr.length}
+          next={fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          {smallArr.map((i, index) => (
+            <div style={{height:"100vh",width:"100%",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}} key={index}>
+              Hello
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
+        
+    
+    )
+}
+
+export default Infi;
